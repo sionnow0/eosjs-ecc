c34cce122f483785ce14f7c2c2aa12390404892c
shionyu <shionyu@163.com>
2019-11-26 19:59:37 +0800

    增加windos node版本安装

--- src/hash_test.js
+++ src/hash_test.js
@@ -1,3 +1,13 @@
+/*
+https://nodejs.org/en/
+node版本如果不够，要用上面这个安装，windos版本的
+node -v
+v12.13.1
+npm -v
+6.13.1
+安装之后的版本如上
+ */
+
 const keyPrivate = require('./key_private')
 const hash = require('./hash');
 const randomBytes = require('randombytes');
